9d8ad4b1abbc8c767d9f836065aa8c4d7336df35
Supachai Chanarittichai <supachaic@outlook.com>
2019-01-08 11:51:41 +0900

    Updates

---
+++ precache-manifest.824bd391e4683785e61be0f5416d8101.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "34e86f06102f347f0475d68f8673318b",
+    "url": "/react-face-recognition/static/media/test.34e86f06.jpg"
+  },
+  {
+    "revision": "2a5d5ea8ef6a7078db61",
+    "url": "/react-face-recognition/static/js/runtime~main.2a5d5ea8.js"
+  },
+  {
+    "revision": "a5265d7d497bc4296c8f",
+    "url": "/react-face-recognition/static/js/main.a5265d7d.chunk.js"
+  },
+  {
+    "revision": "45bf7407ea3ddea65136",
+    "url": "/react-face-recognition/static/js/1.45bf7407.chunk.js"
+  },
+  {
+    "revision": "a5265d7d497bc4296c8f",
+    "url": "/react-face-recognition/static/css/main.dafcc171.chunk.css"
+  },
+  {
+    "revision": "effd922b8fed76002cb15ff13bfcad12",
+    "url": "/react-face-recognition/index.html"
+  }
+];
